ad668f09c4fb68d63577eab787a5ff000b0ebb53
htl-leonding <aenderemich12345>
2020-05-03 02:29:08 +0200

    L03-E19 change Navigation to safe-args

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -34,7 +34,8 @@ class GameWonFragment : Fragment() {
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
         binding.nextMatchButton.setOnClickListener { view ->
-            view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
+            // view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
+            view.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
         }
         var args = GameWonFragmentArgs.fromBundle(arguments!!)
         Toast.makeText(context,
